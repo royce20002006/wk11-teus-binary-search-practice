7656cbba4b461f8de9203b0cfb96fd7558d0b4d2
Royce Calvin <royce20002006@yahoo.com>
2024-02-06 22:22:02 -0600

    finished

--- binary-search.js
+++ binary-search.js
@@ -1,5 +1,5 @@
 function linearSearch (arr, target) {
-
+  return arr.indexOf(target);
   // Can you solve this in one line?
 
 };
@@ -7,21 +7,37 @@ function linearSearch (arr, target) {
 function binarySearch(arr, target) {
 
   // Set integers pointing to the high and low range of possible indices
-
+  let low = 0;
+  let high = arr.length - 1;
   // While high and low indices do not overlap...
-
+  while (low <= high) {
     // Find the midpoint between high and low indices
-
+    let mid = Math.floor((low + high) / 2);
     // Compare the target value to the midpoint value
-
     // If the target equals the midpoint...
+    if (arr[mid] === target) {
       // Return the midpoint index
+      return mid;
+    }
     // If the target is higher than the midpoint...
+    if (arr[mid] < target) {
       // Move the low pointer to midpoint + 1
+      low = mid + 1;
+    }
+    
     // If the target is less than the midpoint...
+    if (arr[mid] > target) {
       // Move the high pointer to midpoint - 1
-
+      high = mid -1;
+    }
+    
+    
+  }
   // Return -1 if the loop exits with overlapping pointers
+  return -1;
+
+
+
 
 }
 
